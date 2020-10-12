c6ae233a43b63b728699ab2864369b64e76dcfdc
Aidan Andrucyk <58194034+aidanandrucyk@users.noreply.github.com>
2020-10-12 00:35:04 -0400

    VAE

--- Python Scripts/low_high_celeb_adversal_test_all.py
+++ Python Scripts/low_high_celeb_adversal_test_all.py
@@ -37,8 +37,8 @@ def save_checkpoint_A(state, filename='Checkpoints/checkpoint_A_0.001.pth.tar'):
 file_path ='Data Sets/list_attr_celeba.txt'
 images_path = 'CelebA/img_align_celeba/'
 
-# CHANGE BACK TO 1
-load_net = 0
+# Testing = 1
+load_net = 1
 
 columns = ['ImgId','5_o_Clock_Shadow', ' Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair', 'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones', 'Male', 'Mouth_Slightly_Open', 'Mustache', 'Narrow_Eyes', 'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline', 'Rosy_Cheeks', 'Sideburns', 'Smiling', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace', 'Wearing_Necktie', 'Young']
 
